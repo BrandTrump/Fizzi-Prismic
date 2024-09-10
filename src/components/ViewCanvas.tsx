a54e238d448754ab26571dd20c86039fc081e051
Brandon Trump <brandontrump101@gmail.com>
2024-09-10 23:23:26 +1200

    add: loader ui from drei

--- src/components/ViewCanvas.tsx
+++ src/components/ViewCanvas.tsx
@@ -2,31 +2,44 @@
 
 import { View } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
 // import { Perf } from "r3f-perf";
+
+import dynamic from "next/dynamic";
+
+const Loader = dynamic(
+  () => import("@react-three/drei").then((mod) => mod.Loader),
+  { ssr: false },
+);
 
 type Props = {};
 
 export default function ViewCanvas({}: Props) {
   return (
-    <Canvas
-      style={{
-        position: "fixed",
-        top: 0,
-        left: "50%",
-        transform: "translateX(-50%)",
-        overflow: "hidden",
-        pointerEvents: "none",
-        zIndex: 30,
-      }}
-      camera={{
-        fov: 30,
-      }}
-      shadows
-      dpr={[1, 1.5]}
-      gl={{ antialias: true }}
-    >
-      <View.Port />
-      {/* <Perf /> */}
-    </Canvas>
+    <>
+      <Canvas
+        style={{
+          position: "fixed",
+          top: 0,
+          left: "50%",
+          transform: "translateX(-50%)",
+          overflow: "hidden",
+          pointerEvents: "none",
+          zIndex: 30,
+        }}
+        camera={{
+          fov: 30,
+        }}
+        shadows
+        dpr={[1, 1.5]}
+        gl={{ antialias: true }}
+      >
+        <Suspense fallback={null}>
+          <View.Port />
+          {/* <Perf /> */}
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </>
   );
 }
